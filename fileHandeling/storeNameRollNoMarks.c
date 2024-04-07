14c5c70055758143e46ba6ab731f55ef10e79fef
smrn001 <samir.niroula001@gmail.com>
2024-04-07 17:50:42 +0545

    file Handling

---
+++ fileHandeling/storeNameRollNoMarks.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main() {
+  char name[15];
+  int rollNo;
+  float marks;
+  printf("Enter name :");
+  fgets(name, 15, stdin);
+  printf("Enter Roll No. :");
+  scanf("%d", &rollNo);
+  printf("Enter Marks :");
+  scanf("%f", &marks);
+  FILE *da;
+  da = fopen("student.txt", "a");
+  printf("Name:%s \tRollNo:%d \tMarks:%.2f \n", name, rollNo, marks);
+  fprintf(da, "\n Name:%s  RollNo:%d \t  Marks:%.2f\n", name, rollNo, marks);
+  fclose(da);
+  return 0;
+}
